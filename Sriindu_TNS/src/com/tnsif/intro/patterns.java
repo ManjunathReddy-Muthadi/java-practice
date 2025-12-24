c2fd035c1d8bcc4adafa7a4210ed95e6f43f5b71
pravalika <pravalika@DESKTOP-18DE0TD>
2025-12-24 18:10:38 +0530

    Pattern program

---
+++ Sriindu_TNS/src/com/tnsif/intro/patterns.java
@@ -0,0 +1,18 @@
+package com.tnsif.intro;
+
+public class patterns {
+	public static void main(String args[])
+	{
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+
+
+}
