37eb2b38df8d680830cee440661584bf57045e1e
pravalika <pravalika@DESKTOP-18DE0TD>
2025-12-24 16:59:23 +0530

    Arithmetic operation

---
+++ Sriindu_TNS/src/com/tnsif/intro/Arithmetic.java
@@ -0,0 +1,19 @@
+package com.tnsif.intro;
+import java.util.*;
+public class Arithmetic {
+	public static void main(String[]args)
+	{
+		Scanner Sc = new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter first number");
+		a = Sc.nextInt();
+		System.out.println("Enter second number");
+		b = Sc.nextInt();
+		int c = a+b;
+		System.out.println("Sum = " + c);
+	}
+	
+	
+	
+
+}
