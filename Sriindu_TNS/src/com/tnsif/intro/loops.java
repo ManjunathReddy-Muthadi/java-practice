d2e40f0962079dab2afdcb78ca3a68d22b45970a
pravalika <pravalika@DESKTOP-18DE0TD>
2025-12-25 18:47:26 +0530

    Loops

---
+++ Sriindu_TNS/src/com/tnsif/intro/loops.java
@@ -0,0 +1,26 @@
+package com.tnsif.intro;
+
+public class loops {
+	public static void main(String[] args)
+	{
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println("HI");
+			
+		}
+		int a=0;
+		while(a<=10)
+		{
+			System.out.println("Hello");
+			a++;
+		}
+		int b=0;
+		do {
+			System.out.println("*");
+			b++;
+		}while(b<=5);
+	}
+	
+	}
+
+
