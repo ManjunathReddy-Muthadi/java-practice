222046c2f0ac4129e1133c407d443a2a25570c2e
pravalika <pravalika@DESKTOP-18DE0TD>
2025-12-26 19:35:14 +0530

    Inheritance

---
+++ Sriindu_TNS/src/com/tnsif/java/inheritance/single.java
@@ -0,0 +1,15 @@
+package com.tnsif.java.inheritance;
+
+public class single {
+  public void show()
+  {
+	  System.out.println("Base class method");
+  }
+  
+}
+class singleInh extends single{
+	public void display()
+	{
+		System.out.println("Derived class");
+	}
+}
---
+++ Sriindu_TNS/src/com/tnsif/java/inheritance/singleDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.java.inheritance;
+
+public class singleDemo {
+	public static void main(String[] args)
+	{
+		singleInh ob = new singleInh();
+		ob.display();
+		ob.show();
+	}
+	
+
+}
