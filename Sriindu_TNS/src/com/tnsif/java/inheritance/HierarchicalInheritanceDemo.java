8a68158e81cbce629d41f8c2b82bfd1a5eadc982
pravalika <pravalika@DESKTOP-18DE0TD>
2026-01-26 12:05:55 +0530

    HierarchicalInheritance

---
+++ Sriindu_TNS/src/com/tnsif/java/inheritance/HierarchicalInheritanceDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.java.inheritance;
+
+public class HierarchicalInheritanceDemo {
+	public static void main(String[]args) {
+		Child1 c1 = new Child1();
+		c1.display();
+		c1.show();
+		Child2 c2 = new Child2();
+		c2.display();
+		c2.show();
+		Child3 c3 = new Child3();
+		c3.display();
+		c3.show();
+				
+				
+	}
+
+}
---
+++ Sriindu_TNS/src/com/tnsif/java/inheritance/HierarchicalInheritence.java
@@ -0,0 +1,23 @@
+package com.tnsif.java.inheritance;
+
+public class HierarchicalInheritence {
+	public void display() {
+		System.out.println("Parent Class Display Method");
+	}
+}
+class Child1 extends HierarchicalInheritence{
+	public void show() {
+		System.out.println("Child1 class show method");
+	}
+}
+class Child2 extends HierarchicalInheritence{
+	public void show() {
+		System.out.println("Child2 class show method");
+	}
+}
+class Child3 extends HierarchicalInheritence{
+	public void show() {
+		System.out.println("Child3 class show method");
+	}
+}
+	
